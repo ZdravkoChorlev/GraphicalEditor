bc5c4c6383d4429f9035e5c78945c42c208abcff
ZdravkoChorlev <30442544+ZdravkoChorlev@users.noreply.github.com>
2018-01-10 01:35:56 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace GraphicalEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button_Rectangle = new System.Windows.Forms.Button();
+            this.button_Circle = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.drawPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button_Rectangle);
+            this.panel1.Controls.Add(this.button_Circle);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(52, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(694, 451);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(402, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "2";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button_Rectangle
+            // 
+            this.button_Rectangle.BackColor = System.Drawing.SystemColors.Control;
+            this.button_Rectangle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_Rectangle.Location = new System.Drawing.Point(131, 12);
+            this.button_Rectangle.Name = "button_Rectangle";
+            this.button_Rectangle.Size = new System.Drawing.Size(94, 28);
+            this.button_Rectangle.TabIndex = 6;
+            this.button_Rectangle.Text = "Rectangle";
+            this.button_Rectangle.UseVisualStyleBackColor = false;
+            this.button_Rectangle.Click += new System.EventHandler(this.button_Rectangle_Click);
+            // 
+            // button_Circle
+            // 
+            this.button_Circle.BackColor = System.Drawing.SystemColors.Control;
+            this.button_Circle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_Circle.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button_Circle.Location = new System.Drawing.Point(251, 12);
+            this.button_Circle.Name = "button_Circle";
+            this.button_Circle.Size = new System.Drawing.Size(94, 28);
+            this.button_Circle.TabIndex = 7;
+            this.button_Circle.Text = "Elipse";
+            this.button_Circle.UseVisualStyleBackColor = false;
+            this.button_Circle.Click += new System.EventHandler(this.button_Elipse_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Control;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(17, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 28);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Line";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(52, 451);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(39, 19);
+            this.exitToolStripMenuItem1.Text = "Clear";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(39, 19);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click_1);
+            // 
+            // drawPanel
+            // 
+            this.drawPanel.AutoSize = true;
+            this.drawPanel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.drawPanel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.drawPanel.Location = new System.Drawing.Point(52, 50);
+            this.drawPanel.Name = "drawPanel";
+            this.drawPanel.Size = new System.Drawing.Size(694, 404);
+            this.drawPanel.TabIndex = 2;
+            this.drawPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel_Draw_MouseDown);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(508, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Pen Size";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 451);
+            this.Controls.Add(this.drawPanel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GraphicalEditor";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Panel drawPanel;
+        private System.Windows.Forms.Button button_Circle;
+        private System.Windows.Forms.Button button_Rectangle;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GraphicalEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            g = drawPanel.CreateGraphics();
+        }
+
+        Graphics g;
+        bool drawRectangle = false;
+        bool drawElipse = false;
+        bool drawLine = false;
+
+        private void panel_Draw_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (drawLine)
+            {
+
+                //Drawing Line
+                Pen p = new Pen(Color.Black, float.Parse(textBox1.Text));
+                Point point1 = new Point(100, 50);
+                Point point2 = new Point(150, 350);
+                g.DrawLine(p, point1, point2);
+               //startPaint = false;
+                drawLine = false;
+            }
+
+            if(drawRectangle)
+            {
+                //Drawing Rectangle
+                Pen p = new Pen(Color.Black, float.Parse(textBox1.Text));
+                g.DrawRectangle(p, 100, 300,50, 70);
+                //startPaint = false;
+                drawRectangle = false;
+            }
+
+            if(drawElipse)
+            {
+                //Drawing Elipse
+                Pen p = new Pen(Color.Black, float.Parse(textBox1.Text));
+                g.DrawEllipse(p, 100, 200, 60, 70);
+                //startPaint = false;
+                drawElipse = false;
+            }
+        }
+
+
+        private void button_Rectangle_Click(object sender, EventArgs e)
+        {
+            drawRectangle = true;
+        }
+
+        private void button_Elipse_Click(object sender, EventArgs e)
+        {
+            drawElipse = true;
+        }   
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            drawLine = true;
+        }
+        //Menu for clear
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            g.Clear(drawPanel.BackColor);
+        }
+        //Menu for exit
+        private void exitToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
